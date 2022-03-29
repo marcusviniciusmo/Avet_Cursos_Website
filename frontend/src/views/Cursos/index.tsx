6cc36260326175815af3f711ab4ed5cc3fbc78fd
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-29 21:20:31 +0100

    Creating the Cursos view structure

--- frontend/src/views/Cursos/index.tsx
+++ frontend/src/views/Cursos/index.tsx
@@ -1,7 +1,90 @@
 function Cursos() {
   return (
     <>
-      Avet - Página Cursos
+      <h1>VIEW</h1>
+      <h1>Avet - Página Cursos</h1>
+
+      <h1>HEADER</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h2>MENU</h2>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Home</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Cursos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Consultas</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>A Avet</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Blog</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Fale Conosco</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Inscrição Online</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Agendar Consulta Online</h4>
+
+      <h1>BODY</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Auxiliar Veterinário</h4>
+      <h5>Completo</h5>
+      <h6>1 Ano</h6>
+      <h5>Módulo I</h5>
+      <h6>6 Meses</h6>
+      <h5>Módulo II</h5>
+      <h6>6 Meses</h6>
+      <h4>Período</h4>
+      <h5>Manhã</h5>
+      <h6>08h às 12h</h6>
+      <h5>Tarde</h5>
+      <h6>13:30 às 17:30</h6>
+      <h5>Noite</h5>
+      <h6>18:30 às 22:30</h6>
+      <h4>Grade Currícular</h4>
+
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Auxiliar em Silvestres e Exóticos</h4>
+      <h6>Duração de 6 Meses</h6>
+      <h4>Período</h4>
+      <h5>Manhã</h5>
+      <h6>08h às 12h</h6>
+      <h5>Tarde</h5>
+      <h6>13:30 às 17:30</h6>
+      <h5>Noite</h5>
+      <h6>18:30 às 22:30</h6>
+      <h4>Grade Currícular</h4>
+
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Auxiliar em Instrumentação e Procedimentos Cirúrgicos</h4>
+      <h6>Duração de 6 Meses</h6>
+      <h4>Período</h4>
+      <h5>Manhã</h5>
+      <h6>08h às 12h</h6>
+      <h5>Tarde</h5>
+      <h6>13:30 às 17:30</h6>
+      <h5>Noite</h5>
+      <h6>18:30 às 22:30</h6>
+      <h4>Grade Currícular</h4>
+
+      <h4>Incluso em todos os cursos</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Apostila</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Certificado</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Jaleco e Estetoscópio</h4>
+
+      <h6>
+        * Uma aula na semana, contendo a parte teórica e prática. ** Aprendizado
+        na prática com atendimentos reais.
+      </h6>
+
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Matricule-se Já</h4>
+
+      <h1>FOOTER</h1>
+      <h4>&copy; 2017 Avet Cursos. Todos os direitos reservados.</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
     </>
   );
 };
