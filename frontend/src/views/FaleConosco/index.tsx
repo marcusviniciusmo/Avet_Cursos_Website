25cc2f696eb6f8680d69ed44f4389a0bc23558d1
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-29 21:42:49 +0100

    Creating the FaleConosco view structure

--- frontend/src/views/FaleConosco/index.tsx
+++ frontend/src/views/FaleConosco/index.tsx
@@ -1,7 +1,80 @@
 function FaleConosco() {
   return (
     <>
-      Avet - Página Fale Conosco
+      <h1>VIEW</h1>
+      <h1>Avet - Página Fale Conosco</h1>
+
+      <h1>HEADER</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h2>MENU</h2>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Home</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Cursos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Consultas</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>A Avet</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Blog</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Fale Conosco</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Inscrição Online</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Agendar Consulta Online</h4>
+
+      <h1>BODY</h1>
+      <h4>Contato</h4>
+      <h5>
+        Entre em contato conosco. Nossa equipe de especialistas está sempre
+        pronta para te atender.
+      </h5>
+
+      <h3>AQUI VAI UMA LINHA</h3>
+
+      <h3>AQUI VAI UM CAMPO</h3>
+      <h4>Nome</h4>
+      <h3>AQUI VAI UM CAMPO</h3>
+      <h4>Telefone</h4>
+      <h3>AQUI VAI UM CAMPO</h3>
+      <h4>E-mail</h4>
+      <h3>AQUI VAI UM CAMPO</h3>
+      <h4>Celular</h4>
+      <h3>AQUI VAI UM CAMPO</h3>
+      <h4>Mensagem</h4>
+
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Enviar</h4>
+
+      <h3>AQUI VAI UMA LINHA</h3>
+
+      <h4>Localização</h4>
+      <h5>
+        Av. Vital Brasil, 302 Jd. Bela Vista - CEP 13076-415 - Campinas - SP
+      </h5>
+      <h3>AQUI VAI UM MAPA</h3>
+
+      <h3>AQUI VAI UMA IMAGEM</h3>
+
+      <h3>AQUI VAI UM ÍCONE</h3>
+      <h5>(19) 3254-4083 // 99900-4489 (Whats)</h5>
+      <h3>AQUI VAI UM ÍCONE</h3>
+      <h5>contato@avetcursos.com.br</h5>
+      <h3>AQUI VAI UM ÍCONE</h3>
+      <h5>Av. Vital Brasil, 302 Jd. Bela Vista - 13076-415 - Campinas/SP</h5>
+
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Whatsapp</h5>
+      <h3>AQUI VAI UM ÍCONE</h3>
+
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Mensagem</h5>
+      <h3>AQUI VAI UM ÍCONE</h3>
+
+      <h1>FOOTER</h1>
+      <h4>&copy; 2017 Avet Cursos. Todos os direitos reservados.</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
     </>
   );
 };
