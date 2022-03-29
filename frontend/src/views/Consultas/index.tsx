7891e5bcf75fd2bab88c39381f3f58113cd1ea28
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-29 21:27:54 +0100

    Creating the Consultas view structure

--- frontend/src/views/Consultas/index.tsx
+++ frontend/src/views/Consultas/index.tsx
@@ -1,7 +1,57 @@
 function Consultas() {
   return (
     <>
-      Avet - Página Consultas
+      <h1>VIEW</h1>
+      <h1>Avet - Página Consultas</h1>
+
+      <h1>HEADER</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h2>MENU</h2>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Home</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Cursos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Consultas</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>A Avet</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Blog</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Fale Conosco</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Inscrição Online</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Agendar Consulta Online</h4>
+
+      <h1>BODY</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Custo Reduzido</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Consulta Particular</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Hospital Escola</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>Profissionais Especializados</h4>
+
+      <h5>Hemograma</h5>
+      <h5>Raio-X</h5>
+      <h5>Exame Loren Ipsum</h5>
+      <h5>Exame Loren Ipsum</h5>
+      <h5>Exame Loren Ipsum</h5>
+
+      <h5>Castração</h5>
+      <h5>Cirurgia Loren Ipsum</h5>
+      <h5>Cirurgia Loren Ipsum</h5>
+      <h5>Cirurgia Loren Ipsum</h5>
+      <h5>Cirurgia Loren Ipsum</h5>
+
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Agende já uma consulta</h4>
+
+      <h1>FOOTER</h1>
+      <h4>&copy; 2017 Avet Cursos. Todos os direitos reservados.</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
     </>
   );
 };
