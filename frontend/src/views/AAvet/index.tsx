a454ccd6d457ec61b741ba5748d5f43997b2266e
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-29 21:34:41 +0100

    Creating the AAvet view structure

--- frontend/src/views/AAvet/index.tsx
+++ frontend/src/views/AAvet/index.tsx
@@ -1,7 +1,61 @@
 function AAvet() {
   return (
     <>
-      Avet - Página A Avet
+      <h1>VIEW</h1>
+      <h1>Avet - Página A Avet</h1>
+
+      <h1>HEADER</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h2>MENU</h2>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Home</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Cursos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Consultas</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>A Avet</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Blog</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Fale Conosco</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Inscrição Online</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Agendar Consulta Online</h4>
+
+      <h1>BODY</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h5>
+        A Avet Cursos detectou essa necessidade e por isso oferece o curso de
+        Auxiliar Veterinário e estágios supervisionados para os apaixonados por
+        cães e gatos.
+        O projeto Avet nasceu há cinco anos, quando a Dra. Maria Arianna começou
+        a lecionar em cursos de Auxiliar Veterinário e sentiu a necessidade de
+        ensinar, orientar e ajudar pessoas que amam animais a cuidar deles. Ao
+        fazer a formação em Auxiliar Veterinário, o profissional sai para o
+        mercado com um bom conhecimento de campo, com amplo conhecimento teórico
+        e principalmente prático de como lidar com a rotina clínica, seja ela em
+        hospitais veterinários, clínicas veterinárias, laboratórios ou abrigos.
+      </h5>
+
+      <h2>GALERIA</h2>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+
+      <h1>FOOTER</h1>
+      <h4>&copy; 2017 Avet Cursos. Todos os direitos reservados.</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
     </>
   );
 };
