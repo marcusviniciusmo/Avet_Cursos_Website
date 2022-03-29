df2e4a8d66a836f6339e3b1579cb80a156fe450f
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-29 21:38:00 +0100

    Creating the Blog view structure

--- frontend/src/views/Blog/index.tsx
+++ frontend/src/views/Blog/index.tsx
@@ -1,7 +1,92 @@
 function Blog() {
   return (
     <>
-      Avet - Página Blog
+      <h1>VIEW</h1>
+      <h1>Avet - Página Blog</h1>
+
+      <h1>HEADER</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h2>MENU</h2>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Home</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Cursos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Consultas</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>A Avet</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Blog</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Fale Conosco</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Inscrição Online</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Agendar Consulta Online</h4>
+
+      <h1>BODY</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>
+        Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020
+      </h4>
+      <h5>
+        Lorem ipsum varius neque et nibh euismod amet lorem phasellus aliquam,
+        conubia rutrum mauris eu pretium vulputate cras molestie. adipiscing non
+        velit sapien rhoncus class nam vehicula taciti curae lacinia duis eu id,
+        curabitur potenti molestie sagittis volutpat tempus feugiat nullam
+        imperdiet molestie taciti duis.
+      </h5>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Ler Mais</h5>
+
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>
+        Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020
+      </h4>
+      <h5>
+        Lorem ipsum varius neque et nibh euismod amet lorem phasellus aliquam,
+        conubia rutrum mauris eu pretium vulputate cras molestie. adipiscing non
+        velit sapien rhoncus class nam vehicula taciti curae lacinia duis eu id,
+        curabitur potenti molestie sagittis volutpat tempus feugiat nullam
+        imperdiet molestie taciti duis.
+      </h5>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Ler Mais</h5>
+
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>
+        Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020
+      </h4>
+      <h5>
+        Lorem ipsum varius neque et nibh euismod amet lorem phasellus aliquam,
+        conubia rutrum mauris eu pretium vulputate cras molestie. adipiscing non
+        velit sapien rhoncus class nam vehicula taciti curae lacinia duis eu id,
+        curabitur potenti molestie sagittis volutpat tempus feugiat nullam
+        imperdiet molestie taciti duis.
+      </h5>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Ler Mais</h5>
+
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>
+        Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020
+      </h4>
+      <h5>
+        Lorem ipsum varius neque et nibh euismod amet lorem phasellus aliquam,
+        conubia rutrum mauris eu pretium vulputate cras molestie. adipiscing non
+        velit sapien rhoncus class nam vehicula taciti curae lacinia duis eu id,
+        curabitur potenti molestie sagittis volutpat tempus feugiat nullam
+        imperdiet molestie taciti duis.
+      </h5>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Ler Mais</h5>
+
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Veja as Matérias</h4>
+
+      <h1>FOOTER</h1>
+      <h4>&copy; 2017 Avet Cursos. Todos os direitos reservados.</h4>
+      <h3>AQUI VAI UMA IMAGEM</h3>
     </>
   );
 };
