2592cbe6fad03ec0c8f3c17fa00879afdd5ed997
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-30 14:23:09 +0100

    Header component creation

---
+++ frontend/src/components/Header/index.tsx
@@ -0,0 +1,27 @@
+function Header() {
+  return (
+    <>
+      <h1>HEADER Component</h1>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h2>MENU</h2>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Home</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Cursos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Consultas</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>A Avet</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Blog</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Fale Conosco</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Inscrição Online</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Agendar Consulta Online</h4>
+    </>
+  );
+};
+
+export default Header;
--- frontend/src/views/InscricaoOnline/index.tsx
+++ frontend/src/views/InscricaoOnline/index.tsx
@@ -1,28 +1,12 @@
+import Header from "components/Header";
+
 function InscricaoOnline() {
   return (
     <>
       <h1>VIEW</h1>
       <h1>Avet - Página Inscrição Online</h1>
 
-      <h1>HEADER</h1>
-      <h3>AQUI VAI UMA IMAGEM</h3>
-      <h2>MENU</h2>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Home</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Cursos</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Consultas</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>A Avet</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Blog</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Fale Conosco</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Inscrição Online</h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Agendar Consulta Online</h4>
+      <Header />
 
       <h1>BODY</h1>
 
