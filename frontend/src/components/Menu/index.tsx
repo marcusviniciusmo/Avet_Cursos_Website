d993112107f32b7ad35ef72d27eebf7534f36e71
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-30 15:06:32 +0100

    Menu component creation

---
+++ frontend/src/components/Menu/index.tsx
@@ -0,0 +1,25 @@
+function Menu() {
+  return (
+    <>
+      <h2>MENU Component</h2>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Home</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Cursos</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Consultas</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>A Avet</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Blog</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Fale Conosco</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Inscrição Online</h4>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Agendar Consulta Online</h4>
+    </>
+  );
+};
+
+export default Menu;
