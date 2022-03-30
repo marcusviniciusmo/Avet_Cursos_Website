8570b5e3a5443b4688ad6f0109562dda54da0a10
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-30 11:50:02 +0100

    Route file creation

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,28 +1,9 @@
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Home from 'views/Home';
-import Cursos from 'views/Cursos';
-import Consultas from 'views/Consultas';
-import AAvet from 'views/AAvet';
-import Blog from 'views/Blog';
-import FaleConosco from 'views/FaleConosco';
-import InscricaoOnline from 'views/InscricaoOnline';
-import AgendarConsultaOnline from 'views/AgendarConsultaOnline';
+import Router from 'routes';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/cursos' element={<Cursos />} />
-        <Route path='/consultas' element={<Consultas />} />
-        <Route path='/aavet' element={<AAvet />} />
-        <Route path='/blog' element={<Blog />} />
-        <Route path='/faleConosco' element={<FaleConosco />} />
-        <Route path='/inscricaoOnline' element={<InscricaoOnline />} />
-        <Route path='/agendarConsultaOnline' element={<AgendarConsultaOnline />} />
-      </Routes>
-    </BrowserRouter>
+    <Router />
   );
 }
 
---
+++ frontend/src/routes/index.tsx
@@ -0,0 +1,28 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Home from 'views/Home';
+import Cursos from 'views/Cursos';
+import Consultas from 'views/Consultas';
+import AAvet from 'views/AAvet';
+import Blog from 'views/Blog';
+import FaleConosco from 'views/FaleConosco';
+import InscricaoOnline from 'views/InscricaoOnline';
+import AgendarConsultaOnline from 'views/AgendarConsultaOnline';
+
+function Router() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/cursos' element={<Cursos />} />
+        <Route path='/consultas' element={<Consultas />} />
+        <Route path='/aavet' element={<AAvet />} />
+        <Route path='/blog' element={<Blog />} />
+        <Route path='/faleConosco' element={<FaleConosco />} />
+        <Route path='/inscricaoOnline' element={<InscricaoOnline />} />
+        <Route path='/agendarConsultaOnline' element={<AgendarConsultaOnline />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default Router;
