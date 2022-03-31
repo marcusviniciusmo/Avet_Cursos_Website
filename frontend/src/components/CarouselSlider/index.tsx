fbe7f7c3afe19fa3ff028632b6191fb8b74b0b92
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-31 20:34:06 +0100

    Added all banners for CarouselSlider component

--- frontend/src/components/CarouselSlider/index.tsx
+++ frontend/src/components/CarouselSlider/index.tsx
@@ -1,5 +1,8 @@
 import { Carousel, CarouselItem } from 'react-bootstrap';
 import SlideAvet01 from 'assets/img/carouselSlider/banner01.png';
+import SlideAvet02 from 'assets/img/carouselSlider/banner02.png';
+import SlideAvet03 from 'assets/img/carouselSlider/banner03.png';
+import SlideAvet04 from 'assets/img/carouselSlider/banner04.png';
 
 function CarouselSlider() {
   return (
@@ -7,6 +10,18 @@ function CarouselSlider() {
       <Carousel>
         <CarouselItem interval={2000}>
           <img src={SlideAvet01} alt="Slide Avet Cursos" />
+        </CarouselItem>
+
+        <CarouselItem interval={2000}>
+          <img src={SlideAvet02} alt="Slide Avet Cursos" />
+        </CarouselItem>
+
+        <CarouselItem interval={2000}>
+          <img src={SlideAvet03} alt="Slide Avet Cursos" />
+        </CarouselItem>
+
+        <CarouselItem interval={2000}>
+          <img src={SlideAvet04} alt="Slide Avet Cursos" />
         </CarouselItem>
       </Carousel>
     </>
