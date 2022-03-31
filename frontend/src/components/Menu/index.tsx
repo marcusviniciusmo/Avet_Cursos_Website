1e70b99475077ee750123f1d5a6c6f39999ef1e1
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-31 14:04:39 +0100

    Navigation through the screens by the menu component

--- frontend/src/components/Menu/index.tsx
+++ frontend/src/components/Menu/index.tsx
@@ -1,25 +1,34 @@
+import { Link } from "react-router-dom";
 import MenuItem from "components/MenuItem";
 
 function Menu() {
   return (
     <>
       <h2>MENU Component</h2>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4><MenuItem textButton="Home" /></h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4><MenuItem textButton="Cursos" /></h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4><MenuItem textButton="Consultas" /></h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4><MenuItem textButton="A Avet" /></h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4><MenuItem textButton="Blog" /></h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4><MenuItem textButton="Fale Conosco" /></h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4><MenuItem textButton="Inscrição Online" /></h4>
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4><MenuItem textButton="Agendar Consulta Online" /></h4>
+      <Link to="/">
+        <h4><MenuItem textButton="Home" /></h4>
+      </Link>
+      <Link to="/cursos">
+        <h4><MenuItem textButton="Cursos" /></h4>
+      </Link>
+      <Link to="/consultas">
+        <h4><MenuItem textButton="Consultas" /></h4>
+      </Link>
+      <Link to="/aavet">
+        <h4><MenuItem textButton="A Avet" /></h4>
+      </Link>
+      <Link to="/blog">
+        <h4><MenuItem textButton="Blog" /></h4>
+      </Link>
+      <Link to="/faleConosco">
+        <h4><MenuItem textButton="Fale Conosco" /></h4>
+      </Link>
+      <Link to="/inscricaoOnline">
+        <h4><MenuItem textButton="Inscrição Online" /></h4>
+      </Link>
+      <Link to="/agendarConsultaOnline">
+        <h4><MenuItem textButton="Agendar Consulta Online" /></h4>
+      </Link>
     </>
   );
 };
