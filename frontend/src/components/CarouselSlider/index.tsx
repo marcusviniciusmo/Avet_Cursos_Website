ddcc700cd1cd68b74e6b00902f8c6548acec33e7
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-31 20:39:50 +0100

    CarouselSlider component styling

--- frontend/src/components/CarouselSlider/index.tsx
+++ frontend/src/components/CarouselSlider/index.tsx
@@ -3,28 +3,42 @@ import SlideAvet01 from 'assets/img/carouselSlider/banner01.png';
 import SlideAvet02 from 'assets/img/carouselSlider/banner02.png';
 import SlideAvet03 from 'assets/img/carouselSlider/banner03.png';
 import SlideAvet04 from 'assets/img/carouselSlider/banner04.png';
+import 'bootstrap/dist/css/bootstrap.css';
+import './styles.css';
 
 function CarouselSlider() {
   return (
-    <>
+    <div id='carouselSliderContainer'>
       <Carousel>
         <CarouselItem interval={2000}>
-          <img src={SlideAvet01} alt="Slide Avet Cursos" />
+          <img
+            className='d-block w-100'
+            src={SlideAvet01}
+            alt="Slide Avet Cursos" />
         </CarouselItem>
 
         <CarouselItem interval={2000}>
-          <img src={SlideAvet02} alt="Slide Avet Cursos" />
+          <img
+            className='d-block w-100'
+            src={SlideAvet02}
+            alt="Slide Avet Cursos" />
         </CarouselItem>
 
         <CarouselItem interval={2000}>
-          <img src={SlideAvet03} alt="Slide Avet Cursos" />
+          <img
+            className='d-block w-100'
+            src={SlideAvet03}
+            alt="Slide Avet Cursos" />
         </CarouselItem>
 
         <CarouselItem interval={2000}>
-          <img src={SlideAvet04} alt="Slide Avet Cursos" />
+          <img
+            className='d-block w-100'
+            src={SlideAvet04}
+            alt="Slide Avet Cursos" />
         </CarouselItem>
       </Carousel>
-    </>
+    </div>
   );
 };
 
