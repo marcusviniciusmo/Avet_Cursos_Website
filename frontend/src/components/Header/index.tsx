bd086cc2464b4bf865cc7014233f5f8055e868de
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-31 15:52:15 +0100

    Static styling of the Header component

--- frontend/src/components/Header/index.tsx
+++ frontend/src/components/Header/index.tsx
@@ -1,9 +1,10 @@
 import LogoAvet from 'assets/img/header/logo.png';
 import Menu from "components/Menu";
+import './styles.css';
 
 function Header() {
   return (
-    <header>
+    <header id='headerContainer'>
       <img src={LogoAvet} alt="Avet logo" />
 
       <Menu />
