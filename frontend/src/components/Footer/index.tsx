210d470157bbca6552cbc28e0142c3dcd5f9ff03
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-31 19:38:27 +0100

    Added pet icon in Footer component using Font Awesome

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,9 +1,12 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faDog } from '@fortawesome/free-solid-svg-icons';
+
 function Footer() {
   return (
     <>
       <h1>FOOTER Component</h1>
       <h4>&copy; 2017 Avet Cursos. Todos os direitos reservados.</h4>
-      <h3>AQUI VAI UMA IMAGEM</h3>
+      <FontAwesomeIcon icon={faDog} />
     </>
   );
 };
