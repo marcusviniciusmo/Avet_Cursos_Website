9c425573ff96da54ada33176811cabb49377c6e9
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-31 19:41:53 +0100

    Creating a global function to get the current year for the Footer component

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,12 +1,21 @@
+import { GetCurrentYear } from 'utils/Functions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDog } from '@fortawesome/free-solid-svg-icons';
 
 function Footer() {
+  const copyright = '\u00a9';
+
   return (
     <>
       <h1>FOOTER Component</h1>
-      <h4>&copy; 2017 Avet Cursos. Todos os direitos reservados.</h4>
-      <FontAwesomeIcon icon={faDog} />
+      <h4>
+        {(`${copyright} ${GetCurrentYear()} Avet Cursos.
+          Todos os direitos reservados.`)}
+      </h4>
+
+      <div id='footerIcon'>
+        <FontAwesomeIcon icon={faDog} />
+      </div>
     </>
   );
 };
--- frontend/src/utils/Functions/index.tsx
+++ frontend/src/utils/Functions/index.tsx
@@ -1,3 +1,9 @@
 export const ToUpperCaseText = (text: string) => {
   return text.toUpperCase();
 };
+
+export const GetCurrentYear = () => {
+  const now = new Date();
+
+  return now.getFullYear();
+};
