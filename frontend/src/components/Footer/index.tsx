a116bceddd8e581baa9ce90360c95ff128182397
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-31 19:47:29 +0100

    Footer component styling

--- frontend/src/components/Footer/index.tsx
+++ frontend/src/components/Footer/index.tsx
@@ -1,22 +1,25 @@
-import { GetCurrentYear } from 'utils/Functions';
+import { GetCurrentYear, ToUpperCaseText } from 'utils/Functions';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faDog } from '@fortawesome/free-solid-svg-icons';
+import './styles.css';
 
 function Footer() {
   const copyright = '\u00a9';
-
   return (
-    <>
-      <h1>FOOTER Component</h1>
-      <h4>
-        {(`${copyright} ${GetCurrentYear()} Avet Cursos.
-          Todos os direitos reservados.`)}
-      </h4>
+    <footer id='footerContainer'>
+      <span>
+        {
+          ToUpperCaseText(
+            `${copyright} ${GetCurrentYear()} Avet Cursos.
+              Todos os direitos reservados.`
+          )
+        }
+      </span>
 
       <div id='footerIcon'>
         <FontAwesomeIcon icon={faDog} />
       </div>
-    </>
+    </footer>
   );
 };
 
