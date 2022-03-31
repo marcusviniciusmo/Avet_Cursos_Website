56143d9e8ebac7ef603a138c90c81b59af83e1b0
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-03-31 15:12:59 +0100

    Alignment and inline display in Menu component stabilization

--- frontend/src/components/Menu/index.tsx
+++ frontend/src/components/Menu/index.tsx
@@ -1,51 +1,53 @@
 import { Link } from "react-router-dom";
 import MenuItem from "components/MenuItem";
+import './styles.css';
 
 function Menu() {
   return (
-    <>
-      <h2>MENU Component</h2>
-      <Link to="/">
-        <h4><MenuItem
-          textButton="Home"
-          id="btnHome" /></h4>
-      </Link>
-      <Link to="/cursos">
-        <h4><MenuItem
-          textButton="Cursos"
-          id="btnCursos" /></h4>
-      </Link>
-      <Link to="/consultas">
-        <h4><MenuItem
-          textButton="Consultas"
-          id="btnConsultas" /></h4>
-      </Link>
-      <Link to="/aavet">
-        <h4><MenuItem
-          textButton="A Avet"
-          id="btnAAvet" /></h4>
-      </Link>
-      <Link to="/blog">
-        <h4><MenuItem
-          textButton="Blog"
-          id="btnBlog" /></h4>
-      </Link>
-      <Link to="/faleConosco">
-        <h4><MenuItem
-          textButton="Fale Conosco"
-          id="btnFaleConosco" /></h4>
-      </Link>
-      <Link to="/inscricaoOnline">
-        <h4><MenuItem
-          textButton="Inscrição Online"
-          id="btnInscricaoOnline" /></h4>
-      </Link>
-      <Link to="/agendarConsultaOnline">
-        <h4><MenuItem
-          textButton="Agendar Consulta Online"
-          id="btnAgendarConsultaOnline" /></h4>
-      </Link>
-    </>
+    <menu id="menuContainer">
+      <ul id="menuList">
+        <Link to="/">
+          <MenuItem
+            textButton="Home"
+            id="btnHome" />
+        </Link>
+        <Link to="/cursos">
+          <MenuItem
+            textButton="Cursos"
+            id="btnCursos" />
+        </Link>
+        <Link to="/consultas">
+          <MenuItem
+            textButton="Consultas"
+            id="btnConsultas" />
+        </Link>
+        <Link to="/aavet">
+          <MenuItem
+            textButton="A Avet"
+            id="btnAAvet" />
+        </Link>
+        <Link to="/blog">
+          <MenuItem
+            textButton="Blog"
+            id="btnBlog" />
+        </Link>
+        <Link to="/faleConosco">
+          <MenuItem
+            textButton="Fale Conosco"
+            id="btnFaleConosco" />
+        </Link>
+        <Link to="/inscricaoOnline">
+          <MenuItem
+            textButton="Inscrição Online"
+            id="btnInscricaoOnline" />
+        </Link>
+        <Link to="/agendarConsultaOnline">
+          <MenuItem
+            textButton="Agendar Consulta Online"
+            id="btnAgendarConsultaOnline" />
+        </Link>
+      </ul>
+    </menu>
   );
 };
 
