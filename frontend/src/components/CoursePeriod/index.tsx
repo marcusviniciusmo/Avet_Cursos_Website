20569308a88258704cd54e5a00a6612f9371ea41
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-04-03 07:34:36 +0100

    CoursePeriod component styling

--- frontend/src/components/CoursePeriod/index.tsx
+++ frontend/src/components/CoursePeriod/index.tsx
@@ -1,21 +1,33 @@
+import { ToUpperCaseText } from 'utils/Functions';
+import './styles.css';
+
 type Props = {
   infoHeader: string,
   infoBody: string
 };
 
 function CoursePeriod({ infoHeader, infoBody }: Props) {
+  const periodCardWidth = 405;
+
   const header = infoHeader.split(',');
   const body = infoBody.split(',');
 
   return (
-    <>
-      <h4>Período</h4>
-      <div>
+    <div id="coursePeriodContainer">
+      <div id='periodLabel'>
+        <span>{ToUpperCaseText('Período')}</span>
+      </div>
+      <div id='coursePeriodInfo'>
         <tr>
           {
             header.map((h) => {
               return (
-                <td>{h}</td>
+                <td
+                  id='periodInfoHeader'
+                  style={{ width: (periodCardWidth / header.length) }}
+                >
+                  {ToUpperCaseText(h)}
+                </td>
               )
             })
           }
@@ -24,14 +36,21 @@ function CoursePeriod({ infoHeader, infoBody }: Props) {
           {
             body.map((b) => {
               return (
-                <td>{b}</td>
+                <td
+                  id='periodInfoBody'
+                  style={{ width: (periodCardWidth / body.length) }}
+                >
+                  {b}
+                </td>
               )
             })
           }
         </tr>
       </div>
-      <h4>Grade Currícular</h4>
-    </>
+      <div id='gradeCurricularLabel'>
+        <span>{ToUpperCaseText('Grade Currícular')}</span>
+      </div>
+    </div>
   );
 };
 
