109d7f912636200ac5f6989d1630e682452e1170
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-04-10 21:17:22 +0100

    Added AAvet view gallery images

--- frontend/src/components/PhotoGallery/index.tsx
+++ frontend/src/components/PhotoGallery/index.tsx
@@ -1,79 +1,95 @@
+import Photo01 from 'assets/img/photoGallery/photo01.png';
+import Photo02 from 'assets/img/photoGallery/photo02.png';
+import Photo03 from 'assets/img/photoGallery/photo03.png';
+import Photo04 from 'assets/img/photoGallery/photo04.png';
+import Photo05 from 'assets/img/photoGallery/photo05.png';
+import Photo06 from 'assets/img/photoGallery/photo06.png';
+import Photo07 from 'assets/img/photoGallery/photo07.png';
+import Photo08 from 'assets/img/photoGallery/photo08.png';
+import Photo09 from 'assets/img/photoGallery/photo09.png';
+import Photo10 from 'assets/img/photoGallery/photo10.png';
+import Photo11 from 'assets/img/photoGallery/photo11.png';
+import Photo12 from 'assets/img/photoGallery/photo12.png';
+import './styles.css';
+
 function PhotoGallery() {
   const pictures = [
     {
       id: 'photo01',
-      imageUrl: '',
+      imageUrl: Photo01,
       imageDescription: 'Galeria Avet - Foto 01'
     },
     {
       id: 'photo02',
-      imageUrl: '',
+      imageUrl: Photo02,
       imageDescription: 'Galeria Avet - Foto 02'
     },
     {
       id: 'photo03',
-      imageUrl: '',
+      imageUrl: Photo03,
       imageDescription: 'Galeria Avet - Foto 03'
     },
     {
       id: 'photo04',
-      imageUrl: '',
+      imageUrl: Photo04,
       imageDescription: 'Galeria Avet - Foto 04'
     },
     {
       id: 'photo05',
-      imageUrl: '',
+      imageUrl: Photo05,
       imageDescription: 'Galeria Avet - Foto 05'
     },
     {
       id: 'photo06',
-      imageUrl: '',
+      imageUrl: Photo06,
       imageDescription: 'Galeria Avet - Foto 06'
     },
     {
       id: 'photo07',
-      imageUrl: '',
+      imageUrl: Photo07,
       imageDescription: 'Galeria Avet - Foto 07'
     },
     {
       id: 'photo08',
-      imageUrl: '',
+      imageUrl: Photo08,
       imageDescription: 'Galeria Avet - Foto 08'
     },
     {
       id: 'photo09',
-      imageUrl: '',
+      imageUrl: Photo09,
       imageDescription: 'Galeria Avet - Foto 09'
     },
     {
       id: 'photo10',
-      imageUrl: '',
+      imageUrl: Photo10,
       imageDescription: 'Galeria Avet - Foto 10'
     },
     {
       id: 'photo11',
-      imageUrl: '',
+      imageUrl: Photo11,
       imageDescription: 'Galeria Avet - Foto 11'
     },
     {
       id: 'photo12',
-      imageUrl: '',
+      imageUrl: Photo12,
       imageDescription: 'Galeria Avet - Foto 12'
     }];
 
   return (
     <>
-      <ul>
-        {
-          pictures.map((picture) => {
-            return (
-              <li id={picture.id}>
-                <span>{picture.imageDescription}</span>
-              </li>
-            )
-          })
-        }
-      </ul>
+      <div id='photoGalleryContainer'>
+        <ul id='photoGalleryList'>
+          {
+            pictures.map((picture) => {
+              return (
+                <li id={picture.id}>
+                  <span>{picture.imageDescription}</span>
+                </li>
+              )
+            })
+          }
+        </ul>
+      </div>
     </>
   );
 };
