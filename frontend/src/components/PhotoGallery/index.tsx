5507692f04aebbc3e5dd6ab8c8d1c01462854be2
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-04-10 21:05:38 +0100

    Creating a logic to display the PhotoGallery according to the amount dynamically

--- frontend/src/components/PhotoGallery/index.tsx
+++ frontend/src/components/PhotoGallery/index.tsx
@@ -1,19 +1,78 @@
 function PhotoGallery() {
+  const pictures = [
+    {
+      id: 'photo01',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 01'
+    },
+    {
+      id: 'photo02',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 02'
+    },
+    {
+      id: 'photo03',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 03'
+    },
+    {
+      id: 'photo04',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 04'
+    },
+    {
+      id: 'photo05',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 05'
+    },
+    {
+      id: 'photo06',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 06'
+    },
+    {
+      id: 'photo07',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 07'
+    },
+    {
+      id: 'photo08',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 08'
+    },
+    {
+      id: 'photo09',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 09'
+    },
+    {
+      id: 'photo10',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 10'
+    },
+    {
+      id: 'photo11',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 11'
+    },
+    {
+      id: 'photo12',
+      imageUrl: '',
+      imageDescription: 'Galeria Avet - Foto 12'
+    }];
+
   return (
     <>
       <ul>
-        <li id="photo01"><span>Galeria Avet - Foto 01</span></li>
-        <li id="photo02"><span>Galeria Avet - Foto 02</span></li>
-        <li id="photo03"><span>Galeria Avet - Foto 03</span></li>
-        <li id="photo04"><span>Galeria Avet - Foto 04</span></li>
-        <li id="photo05"><span>Galeria Avet - Foto 05</span></li>
-        <li id="photo06"><span>Galeria Avet - Foto 06</span></li>
-        <li id="photo07"><span>Galeria Avet - Foto 07</span></li>
-        <li id="photo08"><span>Galeria Avet - Foto 08</span></li>
-        <li id="photo09"><span>Galeria Avet - Foto 09</span></li>
-        <li id="photo10"><span>Galeria Avet - Foto 10</span></li>
-        <li id="photo11"><span>Galeria Avet - Foto 11</span></li>
-        <li id="photo12"><span>Galeria Avet - Foto 12</span></li>
+        {
+          pictures.map((picture) => {
+            return (
+              <li id={picture.id}>
+                <span>{picture.imageDescription}</span>
+              </li>
+            )
+          })
+        }
       </ul>
     </>
   );
