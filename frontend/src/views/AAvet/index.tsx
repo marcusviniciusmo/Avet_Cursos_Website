145fecea123ca4b02044d8017ab7f365d4ecc637
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-04-11 17:59:30 +0100

    AAvet view styling

--- frontend/src/views/AAvet/index.tsx
+++ frontend/src/views/AAvet/index.tsx
@@ -6,36 +6,32 @@ import './styles.css';
 
 function AAvet() {
   return (
-    <>
-      <h1>VIEW</h1>
-      <h1>Avet - Página A Avet</h1>
-
+    <section>
       <Header />
 
-      <h1>BODY</h1>
-      <div id="aavetAreaAbout">
-        <img src={AvetCursosLogo} alt="Avet Cursos logo" />
+      <div id="aavetContainer">
+        <div id="aavetAreaAbout">
+          <img src={AvetCursosLogo} alt="Avet Cursos logo" />
 
-        <span>
-          A Avet Cursos detectou essa necessidade e por isso oferece o curso de
-          Auxiliar Veterinário e estágios supervisionados para os apaixonados por
-          cães e gatos.<br /><br />
-          O projeto Avet nasceu há cinco anos, quando a Dra. Maria Arianna começou
-          a lecionar em cursos de Auxiliar Veterinário e sentiu a necessidade de
-          ensinar, orientar e ajudar pessoas que amam animais a cuidar deles. Ao
-          fazer a formação em Auxiliar Veterinário, o profissional sai para o
-          mercado com um bom conhecimento de campo, com amplo conhecimento teórico
-          e principalmente prático de como lidar com a rotina clínica, seja ela em
-          hospitais veterinários, clínicas veterinárias, laboratórios ou abrigos.
-        </span>
+          <span>
+            A Avet Cursos detectou essa necessidade e por isso oferece o curso de
+            Auxiliar Veterinário e estágios supervisionados para os apaixonados por
+            cães e gatos.<br /><br />
+            O projeto Avet nasceu há cinco anos, quando a Dra. Maria Arianna começou
+            a lecionar em cursos de Auxiliar Veterinário e sentiu a necessidade de
+            ensinar, orientar e ajudar pessoas que amam animais a cuidar deles. Ao
+            fazer a formação em Auxiliar Veterinário, o profissional sai para o
+            mercado com um bom conhecimento de campo, com amplo conhecimento teórico
+            e principalmente prático de como lidar com a rotina clínica, seja ela em
+            hospitais veterinários, clínicas veterinárias, laboratórios ou abrigos.
+          </span>
+        </div>
+
+        <PhotoGallery />
       </div>
 
-      <h2>GALERIA</h2>
-
-      <PhotoGallery />
-
       <Footer />
-    </>
+    </section>
   );
 };
 
