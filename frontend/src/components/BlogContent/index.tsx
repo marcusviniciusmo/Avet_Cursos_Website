d8e94feeca013c2755dc87109688468b82e20a5e
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-04-12 05:12:37 +0100

    BlogContent component creation

---
+++ frontend/src/components/BlogContent/index.tsx
@@ -0,0 +1,66 @@
+function BlogContent() {
+  return (
+    <>
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>
+        Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020
+      </h4>
+      <h5>
+        Lorem ipsum varius neque et nibh euismod amet lorem phasellus aliquam,
+        conubia rutrum mauris eu pretium vulputate cras molestie. adipiscing non
+        velit sapien rhoncus class nam vehicula taciti curae lacinia duis eu id,
+        curabitur potenti molestie sagittis volutpat tempus feugiat nullam
+        imperdiet molestie taciti duis.
+      </h5>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Ler Mais</h5>
+
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>
+        Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020
+      </h4>
+      <h5>
+        Lorem ipsum varius neque et nibh euismod amet lorem phasellus aliquam,
+        conubia rutrum mauris eu pretium vulputate cras molestie. adipiscing non
+        velit sapien rhoncus class nam vehicula taciti curae lacinia duis eu id,
+        curabitur potenti molestie sagittis volutpat tempus feugiat nullam
+        imperdiet molestie taciti duis.
+      </h5>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Ler Mais</h5>
+
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>
+        Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020
+      </h4>
+      <h5>
+        Lorem ipsum varius neque et nibh euismod amet lorem phasellus aliquam,
+        conubia rutrum mauris eu pretium vulputate cras molestie. adipiscing non
+        velit sapien rhoncus class nam vehicula taciti curae lacinia duis eu id,
+        curabitur potenti molestie sagittis volutpat tempus feugiat nullam
+        imperdiet molestie taciti duis.
+      </h5>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Ler Mais</h5>
+
+      <h3>AQUI VAI UMA IMAGEM</h3>
+      <h4>
+        Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020
+      </h4>
+      <h5>
+        Lorem ipsum varius neque et nibh euismod amet lorem phasellus aliquam,
+        conubia rutrum mauris eu pretium vulputate cras molestie. adipiscing non
+        velit sapien rhoncus class nam vehicula taciti curae lacinia duis eu id,
+        curabitur potenti molestie sagittis volutpat tempus feugiat nullam
+        imperdiet molestie taciti duis.
+      </h5>
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h5>Ler Mais</h5>
+
+      <h3>AQUI VAI UM BOTÃO</h3>
+      <h4>Veja as Matérias</h4>
+    </>
+  );
+};
+
+export default BlogContent;
