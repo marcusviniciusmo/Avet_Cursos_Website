114f6ac9a1f8456604de1306f904fb72eac05633
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-04-15 11:23:43 +0100

    Article styling in the BlogContent component

--- frontend/src/components/BlogContent/index.tsx
+++ frontend/src/components/BlogContent/index.tsx
@@ -3,56 +3,59 @@ import FotoMateria2022 from 'assets/img/blog/fotoMateria2022.png';
 import FotoMateria2021 from 'assets/img/blog/fotoMateria2021.png';
 import FotoMateria2020 from 'assets/img/blog/fotoMateria2020.png';
 import FotoMateria2019 from 'assets/img/blog/fotoMateria2019.png';
+import './styles.css';
 
 function BlogContent() {
   return (
     <>
-      <BlogArticle
-        imageUrl={FotoMateria2022}
-        imageDescription="Avet Cursos - logo do artigo"
-        articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2022"
-        articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
-          phasellus aliquam, conubia rutrum mauris eu pretium vulputate cras
-          molestie. adipiscing non velit sapien rhoncus class nam vehicula
+      <div id="blogArticlesContainer">
+        <BlogArticle
+          imageUrl={FotoMateria2022}
+          imageDescription="Avet Cursos - logo do artigo"
+          articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2022"
+          articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
+        phasellus aliquam, conubia rutrum mauris eu pretium vulputate cras
+        molestie. adipiscing non velit sapien rhoncus class nam vehicula
           taciti curae lacinia duis eu id, curabitur potenti molestie sagittis
           volutpat tempus feugiat nullam imperdiet molestie taciti duis.`}
-      />
+        />
 
-      <BlogArticle
-        imageUrl={FotoMateria2021}
-        imageDescription="Avet Cursos - logo do artigo"
-        articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2021"
-        articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
+        <BlogArticle
+          imageUrl={FotoMateria2021}
+          imageDescription="Avet Cursos - logo do artigo"
+          articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2021"
+          articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
           phasellus aliquam, conubia rutrum mauris eu pretium vulputate cras
           molestie. adipiscing non velit sapien rhoncus class nam vehicula
           taciti curae lacinia duis eu id, curabitur potenti molestie sagittis
           volutpat tempus feugiat nullam imperdiet molestie taciti duis.`}
-      />
+        />
 
-      <BlogArticle
-        imageUrl={FotoMateria2020}
-        imageDescription="Avet Cursos - logo do artigo"
-        articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020"
-        articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
+        <BlogArticle
+          imageUrl={FotoMateria2020}
+          imageDescription="Avet Cursos - logo do artigo"
+          articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020"
+          articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
           phasellus aliquam, conubia rutrum mauris eu pretium vulputate cras
           molestie. adipiscing non velit sapien rhoncus class nam vehicula
           taciti curae lacinia duis eu id, curabitur potenti molestie sagittis
           volutpat tempus feugiat nullam imperdiet molestie taciti duis.`}
-      />
+        />
 
-      <BlogArticle
-        imageUrl={FotoMateria2019}
-        imageDescription="Avet Cursos - logo do artigo"
-        articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2019"
-        articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
+        <BlogArticle
+          imageUrl={FotoMateria2019}
+          imageDescription="Avet Cursos - logo do artigo"
+          articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2019"
+          articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
           phasellus aliquam, conubia rutrum mauris eu pretium vulputate cras
           molestie. adipiscing non velit sapien rhoncus class nam vehicula
           taciti curae lacinia duis eu id, curabitur potenti molestie sagittis
           volutpat tempus feugiat nullam imperdiet molestie taciti duis.`}
-      />
+        />
+      </div>
 
       <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Veja as Matérias</h4>
+      <h4>Veja mais Matérias</h4>
     </>
   );
 };
