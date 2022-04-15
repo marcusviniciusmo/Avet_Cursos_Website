a879ad94591ac7f4796750a09065b35b0181e1f2
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-04-15 11:36:42 +0100

    Creating a logic to display the BlogArticles according to the amount dynamically

--- frontend/src/components/BlogContent/index.tsx
+++ frontend/src/components/BlogContent/index.tsx
@@ -7,52 +7,67 @@ import FotoMateria2019 from 'assets/img/blog/fotoMateria2019.png';
 import './styles.css';
 
 function BlogContent() {
+  const articles = [
+    {
+      id: 1,
+      imageUrl: FotoMateria2019,
+      imageDescription: "Avet Cursos - logo do artigo",
+      articleTitle: "Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2019",
+      articleText: `Lorem ipsum varius neque et nibh euismod amet lorem phasellus
+        aliquam, conubia rutrum mauris eu pretium vulputate cras molestie.
+        adipiscing non velit sapien rhoncus class nam vehicula taciti curae
+        lacinia duis eu id, curabitur potenti molestie sagittis volutpat tempus
+        feugiat nullam imperdiet molestie taciti duis.`
+    },
+    {
+      id: 2,
+      imageUrl: FotoMateria2020,
+      imageDescription: "Avet Cursos - logo do artigo",
+      articleTitle: "Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020",
+      articleText: `Lorem ipsum varius neque et nibh euismod amet lorem phasellus
+        aliquam, conubia rutrum mauris eu pretium vulputate cras molestie.
+        adipiscing non velit sapien rhoncus class nam vehicula taciti curae
+        lacinia duis eu id, curabitur potenti molestie sagittis volutpat tempus
+        feugiat nullam imperdiet molestie taciti duis.`
+    },
+    {
+      id: 3,
+      imageUrl: FotoMateria2021,
+      imageDescription: "Avet Cursos - logo do artigo",
+      articleTitle: "Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2021",
+      articleText: `Lorem ipsum varius neque et nibh euismod amet lorem phasellus
+        aliquam, conubia rutrum mauris eu pretium vulputate cras molestie.
+        adipiscing non velit sapien rhoncus class nam vehicula taciti curae
+        lacinia duis eu id, curabitur potenti molestie sagittis volutpat tempus
+        feugiat nullam imperdiet molestie taciti duis.`
+    },
+    {
+      id: 4,
+      imageUrl: FotoMateria2022,
+      imageDescription: "Avet Cursos - logo do artigo",
+      articleTitle: "Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2022",
+      articleText: `Lorem ipsum varius neque et nibh euismod amet lorem phasellus
+        aliquam, conubia rutrum mauris eu pretium vulputate cras molestie.
+        adipiscing non velit sapien rhoncus class nam vehicula taciti curae
+        lacinia duis eu id, curabitur potenti molestie sagittis volutpat tempus
+        feugiat nullam imperdiet molestie taciti duis.`
+    }];
+
   return (
     <div id="blogContentContainer">
       <div id="blogArticlesContainer">
-        <BlogArticle
-          imageUrl={FotoMateria2022}
-          imageDescription="Avet Cursos - logo do artigo"
-          articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2022"
-          articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
-            phasellus aliquam, conubia rutrum mauris eu pretium vulputate cras
-            molestie. adipiscing non velit sapien rhoncus class nam vehicula
-            taciti curae lacinia duis eu id, curabitur potenti molestie sagittis
-            volutpat tempus feugiat nullam imperdiet molestie taciti duis.`}
-        />
-
-        <BlogArticle
-          imageUrl={FotoMateria2021}
-          imageDescription="Avet Cursos - logo do artigo"
-          articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2021"
-          articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
-            phasellus aliquam, conubia rutrum mauris eu pretium vulputate cras
-            molestie. adipiscing non velit sapien rhoncus class nam vehicula
-            taciti curae lacinia duis eu id, curabitur potenti molestie sagittis
-            volutpat tempus feugiat nullam imperdiet molestie taciti duis.`}
-        />
-
-        <BlogArticle
-          imageUrl={FotoMateria2020}
-          imageDescription="Avet Cursos - logo do artigo"
-          articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020"
-          articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
-            phasellus aliquam, conubia rutrum mauris eu pretium vulputate cras
-            molestie. adipiscing non velit sapien rhoncus class nam vehicula
-            taciti curae lacinia duis eu id, curabitur potenti molestie sagittis
-            volutpat tempus feugiat nullam imperdiet molestie taciti duis.`}
-        />
-
-        <BlogArticle
-          imageUrl={FotoMateria2019}
-          imageDescription="Avet Cursos - logo do artigo"
-          articleTitle="Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2019"
-          articleText={`Lorem ipsum varius neque et nibh euismod amet lorem
-            phasellus aliquam, conubia rutrum mauris eu pretium vulputate cras
-            molestie. adipiscing non velit sapien rhoncus class nam vehicula
-            taciti curae lacinia duis eu id, curabitur potenti molestie sagittis
-            volutpat tempus feugiat nullam imperdiet molestie taciti duis.`}
-        />
+        {
+          articles.map((article) => {
+            return (
+              <BlogArticle
+                imageUrl={article.imageUrl}
+                imageDescription={article.imageDescription}
+                articleTitle={article.articleTitle}
+                articleText={article.articleText}
+              />
+            )
+          })
+        }
       </div>
 
       <div id="btnVejaMaisMaterias">
