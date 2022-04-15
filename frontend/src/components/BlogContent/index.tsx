9e6298c18750a2c2e8525749a65f14e487518718
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-04-15 11:49:11 +0100

    Creation of a state that controls the number of articles to be displayed

--- frontend/src/components/BlogContent/index.tsx
+++ frontend/src/components/BlogContent/index.tsx
@@ -1,15 +1,32 @@
+import { useState } from "react";
 import { ToUpperCaseText } from "utils/Functions";
 import BlogArticle from "components/BlogArticle";
 import FotoMateria2022 from 'assets/img/blog/fotoMateria2022.png';
 import FotoMateria2021 from 'assets/img/blog/fotoMateria2021.png';
 import FotoMateria2020 from 'assets/img/blog/fotoMateria2020.png';
 import FotoMateria2019 from 'assets/img/blog/fotoMateria2019.png';
+import FotoMateria2018 from 'assets/img/blog/fotoMateria2018.png';
 import './styles.css';
 
 function BlogContent() {
+  const numberArticlesDisplayed = 4;
+
+  const [showAll, setShowAll] = useState(false);
+
   const articles = [
     {
       id: 1,
+      imageUrl: FotoMateria2018,
+      imageDescription: "Avet Cursos - logo do artigo",
+      articleTitle: "Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2018",
+      articleText: `Lorem ipsum varius neque et nibh euismod amet lorem phasellus
+        aliquam, conubia rutrum mauris eu pretium vulputate cras molestie.
+        adipiscing non velit sapien rhoncus class nam vehicula taciti curae
+        lacinia duis eu id, curabitur potenti molestie sagittis volutpat tempus
+        feugiat nullam imperdiet molestie taciti duis.`
+    },
+    {
+      id: 2,
       imageUrl: FotoMateria2019,
       imageDescription: "Avet Cursos - logo do artigo",
       articleTitle: "Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2019",
@@ -20,7 +37,7 @@ function BlogContent() {
         feugiat nullam imperdiet molestie taciti duis.`
     },
     {
-      id: 2,
+      id: 3,
       imageUrl: FotoMateria2020,
       imageDescription: "Avet Cursos - logo do artigo",
       articleTitle: "Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2020",
@@ -31,7 +48,7 @@ function BlogContent() {
         feugiat nullam imperdiet molestie taciti duis.`
     },
     {
-      id: 3,
+      id: 4,
       imageUrl: FotoMateria2021,
       imageDescription: "Avet Cursos - logo do artigo",
       articleTitle: "Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2021",
@@ -42,7 +59,7 @@ function BlogContent() {
         feugiat nullam imperdiet molestie taciti duis.`
     },
     {
-      id: 4,
+      id: 5,
       imageUrl: FotoMateria2022,
       imageDescription: "Avet Cursos - logo do artigo",
       articleTitle: "Sucesso na Carreira: 5 dicas para auxiliar veterinários em 2022",
@@ -53,25 +70,47 @@ function BlogContent() {
         feugiat nullam imperdiet molestie taciti duis.`
     }];
 
+  const handleShowAll = () => {
+    setShowAll(!showAll);
+  };
+
   return (
     <div id="blogContentContainer">
       <div id="blogArticlesContainer">
         {
-          articles.map((article) => {
-            return (
-              <BlogArticle
-                imageUrl={article.imageUrl}
-                imageDescription={article.imageDescription}
-                articleTitle={article.articleTitle}
-                articleText={article.articleText}
-              />
-            )
-          })
+          showAll
+            ? articles.reverse().map((article) => {
+              return (
+                <BlogArticle
+                  imageUrl={article.imageUrl}
+                  imageDescription={article.imageDescription}
+                  articleTitle={article.articleTitle}
+                  articleText={article.articleText}
+                />
+              )
+            })
+            : articles.reverse()
+              .filter(article => article.id > (
+                articles.length - numberArticlesDisplayed))
+              .map((article) => {
+                return (
+                  <BlogArticle
+                    imageUrl={article.imageUrl}
+                    imageDescription={article.imageDescription}
+                    articleTitle={article.articleTitle}
+                    articleText={article.articleText}
+                  />
+                )
+              })
         }
       </div>
 
-      <div id="btnVejaMaisMaterias">
-        <span>{ToUpperCaseText('Veja mais Matérias')}</span>
+      <div id="btnVejaMaisMaterias" onClick={handleShowAll}>
+        {
+          showAll
+            ? <span>{ToUpperCaseText('Veja menos Matérias')}</span>
+            : <span>{ToUpperCaseText('Veja mais Matérias')}</span>
+        }
       </div>
     </div>
   );
