f498ef5569c9b500f6dcce197e6011fbfb8f01e2
Marcus Vinícius <marcus.viniciusmo@hotmail.com>
2022-04-26 06:24:41 +0100

    ContactForm component styling

--- frontend/src/components/ContactForm/index.tsx
+++ frontend/src/components/ContactForm/index.tsx
@@ -1,22 +1,77 @@
+import { ToUpperCaseText } from "utils/Functions";
+import './styles.css';
+
 function ContactForm() {
   return (
-    <>
-      <h3>AQUI VAI UMA LINHA</h3>
+    <div id="contactFormContainer">
+      <div id="contactFormBorder" />
 
-      <h3>AQUI VAI UM CAMPO</h3>
-      <h4>Nome</h4>
-      <h3>AQUI VAI UM CAMPO</h3>
-      <h4>Telefone</h4>
-      <h3>AQUI VAI UM CAMPO</h3>
-      <h4>E-mail</h4>
-      <h3>AQUI VAI UM CAMPO</h3>
-      <h4>Celular</h4>
-      <h3>AQUI VAI UM CAMPO</h3>
-      <h4>Mensagem</h4>
+      <form action="">
+        <div className="contactFormRow">
+          <div className="contactFormField">
+            <input
+              type="text"
+              name="nameField"
+              id="nameField"
+              maxLength={50}
+              placeholder="Nome"
+            />
+          </div>
 
-      <h3>AQUI VAI UM BOTÃO</h3>
-      <h4>Enviar</h4>
-    </>
+          <div className="contactFormField">
+            <input
+              type="tel"
+              name="phoneField"
+              id="phoneField"
+              maxLength={20}
+              placeholder="Telefone"
+            />
+          </div>
+        </div>
+
+        <div className="contactFormRow">
+          <div className="contactFormField">
+            <input
+              type="email"
+              name="emailField"
+              id="emailField"
+              maxLength={50}
+              placeholder="E-mail"
+            />
+          </div>
+
+          <div className="contactFormField">
+            <input
+              type="tel"
+              name="cellPhoneField"
+              id="cellPhoneField"
+              maxLength={20}
+              placeholder="Celular"
+            />
+          </div>
+        </div>
+
+        <div className="contactFormRow">
+          <div className="contactFormField">
+            <textarea
+              name="messageField"
+              id="messageField"
+              cols={30}
+              rows={5}
+              maxLength={1000}
+              placeholder="Mensagem"
+            />
+          </div>
+        </div>
+
+        <div id="contactFormBtnEnviar">
+          <button
+            type="submit">
+            {ToUpperCaseText('Enviar')}
+          </button>
+        </div>
+      </form>
+    </div>
   );
 };
 
